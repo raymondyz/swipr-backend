f5a64d491df8e83b9582b6412da43265d6033a94
lnhuangg <lesliehuang@g.ucla.edu>
2026-05-16 16:58:21 -0700

    Made g.ucla and ucla to be functionally the same email

--- services/authService.js
+++ services/authService.js
@@ -5,12 +5,14 @@ import { sendVerificationEmail } from "./emailService.js";
 
 export async function signup({ name, username, email, password }) {
   const passwordHash = await bcrypt.hash(password, 10);
+  const standardEmail = standardizeEmail(email)
+  console.log(standardEmail)
 
   try {
     return await createUser({
       name,
       username,
-      email,
+      email: standardEmail,
       password_hash: passwordHash,
       role: "user",
     });
@@ -27,9 +29,10 @@ export async function signup({ name, username, email, password }) {
 
 export async function login(email, password) {
   let user;
+  const standardEmail = standardizeEmail(email)
 
   try {
-    user = await getUserByEmail(email);
+    user = await getUserByEmail(standardEmail);
   } catch (err) {
     throw new Error("Invalid credentials");
   }
@@ -51,19 +54,30 @@ function generateCode() {
   return Math.floor(100000 + Math.random() * 900000).toString();
 }
 
+export function standardizeEmail(email) {
+  email = email.trim().toLowerCase()
+  
+  if (email.endsWith("@g.ucla.edu"))
+    return email.replace("@g.ucla.edu", "@ucla.edu")
+
+  return email
+}
+
 export async function createAndSendCode(email) {
   const code = generateCode()
-  const user = await getUserByEmail(email)
+  const standardEmail = standardizeEmail(email)
+  const user = await getUserByEmail(standardEmail)
   if (!user) {
     throw new Error("User not found")
   }
 
   await setVerificationCode(user.id, code, 10 * 60 * 1000)
-  await sendVerificationEmail(email, code)
+  await sendVerificationEmail(standardEmail, code)
 }
 
 export async function verifyCodeAndActivate(email, code) {
-  const user = await getUserByEmail(email)
+  const standardEmail = standardizeEmail(email)
+  const user = await getUserByEmail(standardEmail)
   if (!user) throw new Error()
 
   await verifyCode(user.id, code)
