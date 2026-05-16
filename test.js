6e1617734e541b0af686bf470e9e0146d6088a45
lnhuangg <lesliehuang@g.ucla.edu>
2026-05-16 16:58:21 -0700

    Made g.ucla and ucla to be functionally the same email

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 import { sendVerificationEmail } from "./services/emailService.js";
 import { createAndSendCode, signup, verifyCodeAndActivate } from "./services/authService.js";
+import { standardizeEmail } from "./services/authService.js";
 
 
 // createAndSendCode("ryz@ucla.edu")
-verifyCodeAndActivate("ryz@ucla.edu", "390996")
+console.log(standardizeEmail("RYz@g.ucla.edu"));
 
 // signup({ name: "raymond", username: "ryz", email: "ryz@ucla.edu", password: "raymond123"})
