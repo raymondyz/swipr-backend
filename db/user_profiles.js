e732061243367b0d7fa3fe1c3a6b154aef95f68f
Raymond Zou <raymond.yh.zou@gmail.com>
2026-05-22 21:13:20 -0700

    Refactor profiles

--- db/user_profiles.js
+++ db/user_profiles.js
@@ -1,5 +1,27 @@
 import { supabase } from "./client.js"
 
+export async function getAllUserProfiles() {
+    const {data, error} = await supabase
+        .from("user_profiles")
+        .select(`
+        user_id,
+        swipe_availability,
+        notes,
+        location_preferences,
+        users (
+            name,
+            username
+        )
+    `);
+
+    if (error) {
+        console.error("Error fetching user profiles:", error);
+        return { data: null, error };
+    }
+
+    return { data, error: null };
+}
+
 export async function getProfile(userId) {
   const { data, error } = await supabase
     .from("user_profiles")
--- db/users.js
+++ db/users.js
@@ -50,25 +50,3 @@ export async function updateUser(userId, updates) {
 export function activateUser(userId) {
   return updateUser(userId, { is_verified: true })
 }
-
-export async function getAllUserProfiles() {
-    const {data, error} = await supabase
-        .from("user_profiles")
-        .select(`
-        user_id,
-        swipe_availability,
-        notes,
-        location_preferences,
-        users (
-            name,
-            username
-        )
-    `);
-
-    if (error) {
-        console.error("Error fetching user profiles:", error);
-        return { data: null, error };
-    }
-
-    return { data, error: null };
-}
